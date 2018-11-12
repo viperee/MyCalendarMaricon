5e3430289b1bfc288a06089d9b1c93949698a8ba
viperee <just.b.nd.l@gmail.com>
2018-11-12 12:00:13 +0100

    #1 Initialisation

---
+++ Back/mycalendarmaricon/src/main/java/fr/mycalendarmaricon/mycalendarmaricon/MycalendarmariconApplication.java
@@ -0,0 +1,12 @@
+package fr.mycalendarmaricon.mycalendarmaricon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycalendarmariconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycalendarmariconApplication.class, args);
+	}
+}
---
+++ Back/mycalendarmaricon/src/test/java/fr/mycalendarmaricon/mycalendarmaricon/MycalendarmariconApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.mycalendarmaricon.mycalendarmaricon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MycalendarmariconApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
