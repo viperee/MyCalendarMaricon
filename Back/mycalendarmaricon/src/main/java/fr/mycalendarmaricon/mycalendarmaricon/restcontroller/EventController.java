e7c2b20b7d9dcc429b6f0230bab162f1079b009d
viperee <just.b.nd.l@gmail.com>
2018-12-17 16:57:31 +0100

    Config Swagger - API REST DOC

--- Back/mycalendarmaricon/src/main/java/fr/mycalendarmaricon/mycalendarmaricon/restcontroller/EventController.java
+++ Back/mycalendarmaricon/src/main/java/fr/mycalendarmaricon/mycalendarmaricon/restcontroller/EventController.java
@@ -17,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.mycalendarmaricon.mycalendarmaricon.exception.EventNotFoundException;
 import fr.mycalendarmaricon.mycalendarmaricon.model.Event;
 import fr.mycalendarmaricon.mycalendarmaricon.service.EventService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
+@Api(value="Event", description="Operations pertaining to event in MyCalendar")
 public class EventController {
 	@Autowired
 	private EventService eventService;
@@ -27,6 +32,10 @@ public class EventController {
 	public static final String URL_EVENTS = "/events";
 	public static final String URL_EVENTS_WITH_ID = URL_EVENTS + "/{id}";
 	
+	@ApiOperation(value = "View a list of events")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+})
 	@GetMapping(URL_EVENTS)
 	public ResponseEntity<List<Event>> getAllEvents(){
 		List<Event> res = eventService.getAllEvents();
@@ -34,12 +43,21 @@ public class EventController {
 		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "Create an event")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Event created"),
+})
 	@PutMapping(URL_EVENTS)
 	public ResponseEntity<Event> createEvent(@RequestBody Event evt){
 		Event eventSaved = eventService.saveEvt(evt);
 		return new ResponseEntity<>(eventSaved, HttpStatus.CREATED);
 	}
 	
+	@ApiOperation(value = "View an event info")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved event"),
+            @ApiResponse(code = 404, message = "No event in db"),
+})
 	@GetMapping(URL_EVENTS_WITH_ID)
 	public ResponseEntity<Event> getEventById(@PathVariable(name = "id") Long id){
 		try {
@@ -50,6 +68,11 @@ public class EventController {
 		}
 	}
 	
+	@ApiOperation(value = "Delete an event")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Event deleted"),
+            @ApiResponse(code = 404, message = "Event not found in database"),
+})
 	@DeleteMapping(URL_EVENTS_WITH_ID)
 	public ResponseEntity<Event> deleteEventById(@PathVariable(name = "id") Long id){
 		boolean isDeleted = eventService.deleteById(id);
@@ -61,6 +84,11 @@ public class EventController {
 	}
 	
 	// Mettre à jour un evenement par id
+	@ApiOperation(value = "Update an event")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Event updated"),
+            @ApiResponse(code = 404, message = "Event not found in database"),
+})
 	@PostMapping(URL_EVENTS_WITH_ID)
 	public ResponseEntity<Event> updateEventsById(@PathVariable(name = "id") Long id,@RequestBody Event event){
 		//identifiant correct?
